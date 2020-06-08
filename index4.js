d08d3ceafa0341cabe03fbe32c03c746a598622b
Kalpana Padalkar <kalpana.padalkar@lrn.com>
2020-06-08 11:49:12 +0530

    committed changes for testing

--- index4.js
+++ index4.js
@@ -10,7 +10,7 @@ shellJs.cd(repoPath);
 shellJs.exec(`git checkout ${baseBranch}`);
 shellJs.exec(`git pull origin ${baseBranch}`);
 shellJs.exec(`git add .`);
-if (shellJs.exec('git commit -am "Auto-commit"').code !== 0) {
+if (shellJs.exec('git commit -am "committed changes for testing"').code !== 0) {
     shellJs.echo('Error: Git commit failed');
     shellJs.exit(1);
   }
