b4ec91cca1c4835612e79b98c9df2f37f8d67b3a
Kalpana Padalkar <kalpana.padalkar@lrn.com>
2020-06-08 13:13:03 +0530

    Added changes for testing

--- index5.js
+++ index5.js
@@ -12,25 +12,28 @@ const dirPath = __dirname;
 
 // console.log(dirPath);
 
-exec('cd ${dirPath}', function(err, stdout, stderr) {
-        console.log("cd: " + err + " : "  + stdout);
-        exec("pwd", function(err, stdout, stderr) {
-            console.log("pwd: " + err + " : " + stdout);
-            exec("git status", function(err, stdout, stderr) {
-                console.log("git status returned " ); console.log(err);
-            })
-            exec("git pull", function(err, stdout, stderr) {
-                console.log("git pull success" ); console.log(stdout);
-            })
-            exec("git add .", function(err, stdout, stderr) {
-                console.log("git added success" ); console.log(stdout);
-            })
+exec(`cd ${dirPath} && git checkout master && git pull && git add . && git commit -am "Added changes for testing" && git push origin master`)
+// exec(`cd ${dirPath} && git reset --hard && git clean -f && git checkout master && git pull && git add . && git commit -am "Added changes for testing" && git push origin master`)
 
-            exec("git commit -am 'committed changes for testing for exec'", function(err, stdout, stderr) {
-                console.log("git committed success" ); console.log(stdout);
-            })
-            exec("git push", function(err, stdout, stderr) {
-                console.log("git push success" ); console.log(stdout);
-            })
-        })
-    })
+// exec('cd ${dirPath}', function(err, stdout, stderr) {
+//         console.log("cd: " + err + " : "  + stdout);
+//         exec("pwd", function(err, stdout, stderr) {
+//             console.log("pwd: " + err + " : " + stdout);
+//             exec("git status", function(err, stdout, stderr) {
+//                 console.log("git status returned " ); console.log(err);
+//             })
+//             exec("git pull", function(err, stdout, stderr) {
+//                 console.log("git pull done" ); console.log(stdout);
+//             })
+//             exec("git add .", function(err, stdout, stderr) {
+//                 console.log("git added files done" ); console.log(stdout);
+//             })
+
+//             exec("git commit -am 'committed changes for testing for exec'", function(err, stdout, stderr) {
+//                 console.log("git committed successfully done" ); console.log(stdout);
+//             })
+//             exec("git push", function(err, stdout, stderr) {
+//                 console.log("git push done" ); console.log(stdout);
+//             })
+//         })
+//     })
